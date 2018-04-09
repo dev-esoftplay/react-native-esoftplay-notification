d8dd88afe3bbff15ba9711bd51d7e803a7dcce2e
kholil <kholil@fisip.net>
2018-04-09 10:38:47 +0700

    create module

---
+++ index.js
@@ -0,0 +1,59 @@
+import Permissions from '../../node_modules/expo/src/Permissions.js';
+import Notifications from '../../node_modules/expo/src/Notifications.js';
+import { configConsole } from '../config';
+import { AsyncStorage } from 'react-native';
+
+/*
+{ 
+  to: // exp token
+  data:{
+    action: // [route, dialog,  ]
+    route_name: // routeName of ReactNavigation
+    params: 
+    title:
+    message:
+  } 
+  title:
+  body:
+  sound:
+  param:
+  ttl:
+  expiration:
+  priority:
+  badge:
+} */
+
+const EsoftplayNotification = {
+  listen(callback) {
+    Notifications.addListener((obj) => {
+      if (obj) {
+        callback(obj)
+        if (obj.remote) {
+          AsyncStorage.setItem('esoftplaynotification', JSON.stringify(obj))
+        }
+      }
+    })
+  },
+  get(action) {
+    setTimeout(() => {
+      AsyncStorage.getItem('esoftplaynotification').then((res) => {
+        res = JSON.parse(res)
+        action(res)
+        AsyncStorage.removeItem('esoftplaynotification')
+      }, 1500);
+    })
+  },
+  async requestPermission(token) {
+    const { existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') return
+    configConsole(finalStatus)
+    token(await Notifications.getExpoPushTokenAsync())
+  }
+}
+
+export default EsoftplayNotification
